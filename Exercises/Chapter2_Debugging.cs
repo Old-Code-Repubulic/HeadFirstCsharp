6c62246afc9cd9b80884e47244f7ba032144a1fd
joseph tidwell <joseph.tidwell09@gmail.com>
2022-01-05 19:51:36 -0600

    created small loop to practice the debugging portion of loops in chapter

---
+++ Exercises/Chapter2_Debugging.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ch._2_coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string sentence = "hidey oh";
+            int num = 0; 
+            while (sentence != "")
+            {
+                Console.WriteLine("I'm not an empy string");
+                if (num == 3)
+                    sentence = "";
+                else
+                {
+                    num++;
+                }
+            }
+
+        }
+    }
+}
