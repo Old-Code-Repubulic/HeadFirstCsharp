3be92a154a6ee9ff4a869f07923a142ba054a7af
joseph tidwell <joseph.tidwell09@gmail.com>
2022-01-16 16:53:39 -0600

    moved projected over to Ch.2 repo

---
+++ chapter2/Ch.2 coding/Ch.2 coding/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ch._2_coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string sentence = "hidey oh";
+            int num = 0; 
+            while (sentence != "")
+            {
+                Console.WriteLine("I'm not an empy string");
+                if (num == 3)
+                    sentence = "";
+                else
+                {
+                    num++;
+                }
+            }
+
+            TrySomeLoops();
+        }
+
+
+        private static void TrySomeLoops()
+        {
+            int count = 0;
+            while (count < 10)
+            {
+                count = count + 1;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                count = count - 1;
+            }
+            Console.WriteLine("The answer is " + count);
+        }
+    }
+}
